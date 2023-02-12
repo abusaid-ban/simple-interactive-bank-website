b72627866091b42c7c99b665d30910c34e8d5055
unknown <personal.abusaid@gmail.com>
2023-02-12 14:42:25 +0600

    complete simple website by using event and dom

---
+++ js/withdraw.js
@@ -0,0 +1,19 @@
+document.getElementById('button-withdraw').addEventListener('click',function(){
+    const withdrawField = document.getElementById('withdraw-amount');
+    const newWithdrawValueString = withdrawField.value ;
+    newWithdrawValue = parseFloat(newWithdrawValueString);
+    const withdrawTotalElement = document.getElementById('withdraw-total');
+    const previousWithdrawTotalString = withdrawTotalElement.innerText;
+    const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
+    const currentTotal = previousWithdrawTotal + newWithdrawValue;
+    withdrawTotalElement.innerText =  currentTotal;
+
+    const balanceField = document.getElementById("total-balance");
+    const previousBalanceTotalString = balanceField.innerText;
+    previousBalanceTotal = parseFloat(previousBalanceTotalString);
+    const currentBalanceTotal = previousBalanceTotal - newWithdrawValue;
+    balanceField.innerText = currentBalanceTotal;
+
+    
+    withdrawField.value ="";
+})
