3bdc27084025c66f19d12bc91fa1ddb969c1cf81
unknown <personal.abusaid@gmail.com>
2023-02-12 15:04:45 +0600

    validation check

--- js/withdraw.js
+++ js/withdraw.js
@@ -1,25 +1,30 @@
-document.getElementById('button-withdraw').addEventListener('click',function(){
+document.getElementById('button-withdraw').addEventListener('click', function () {
     const withdrawField = document.getElementById('withdraw-amount');
-    const newWithdrawValueString = withdrawField.value ;
-    newWithdrawValue = parseFloat(newWithdrawValueString);
+    const newWithdrawValueString = withdrawField.value;
+     const newWithdrawValue = parseFloat(newWithdrawValueString);
+     withdrawField.value = "";
+     if(isNaN(newWithdrawValue)){
+        alert('please provide a valid number');
+        return;
+     }
+
     const withdrawTotalElement = document.getElementById('withdraw-total');
     const previousWithdrawTotalString = withdrawTotalElement.innerText;
     const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
     const currentTotal = previousWithdrawTotal + newWithdrawValue;
-    withdrawTotalElement.innerText =  currentTotal;
+    withdrawTotalElement.innerText = currentTotal;
 
     const balanceField = document.getElementById("total-balance");
     const previousBalanceTotalString = balanceField.innerText;
-     const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+    const previousBalanceTotal = parseFloat(previousBalanceTotalString);
 
-    if(newWithdrawValue > previousBalanceTotal){
+   
+
+    if (newWithdrawValue > previousBalanceTotal) {
         alert('insufficient balance');
         return;
 
     }
     const currentBalanceTotal = previousBalanceTotal - newWithdrawValue;
     balanceField.innerText = currentBalanceTotal;
-
-    
-    withdrawField.value ="";
 })
